0dc4e12fb06039f5d6a7b615c644b185dee83691
Jeremias <jeremias.ramirez@davinci.edu.ar>
2025-06-28 07:24:23 -0300

    agrega header con su estilo

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <router-link to="/" class="logo">🎬 Películas App</router-link>
+      <nav class="nav">
+        <router-link to="/" class="nav-link">Inicio</router-link>
+        <router-link to="/favoritos" class="nav-link">Favoritos</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+</script>
