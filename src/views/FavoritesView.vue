d2e1ca4e526114fe58a224dd8ac5718a6924cd4f
Jeremias <jeremias.ramirez@davinci.edu.ar>
2025-06-28 09:20:20 -0300

    boton de eliminar de favoritos para cada tarjeta y su estilo

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -2,16 +2,20 @@
   <div class="favorites">
     <h1>Mis Favoritos</h1>
     <div v-if="favorites.length > 0" class="favorites-grid">
-      <router-link
+      <div
         v-for="movie in favorites"
         :key="movie.id"
-        :to="`/pelicula/${movie.id}`"
         class="favorite-card"
       >
-        <img :src="getImageUrl(movie.poster_path)" :alt="movie.title" />
-        <h2>{{ movie.title }}</h2>
-        <p>Estreno: {{ movie.release_date }}</p>
-      </router-link>
+        <router-link :to="`/pelicula/${movie.id}`">
+          <img :src="getImageUrl(movie.poster_path)" :alt="movie.title" />
+          <h2>{{ movie.title }}</h2>
+          <p>Estreno: {{ movie.release_date }}</p>
+        </router-link>
+        <button @click="removeFromFavorites(movie.id)" class="btn-remove">
+          Eliminar de favoritos
+        </button>
+      </div>
     </div>
     <p v-else class="no-favorites">No hay películas en favoritos.</p>
   </div>
@@ -24,8 +28,16 @@ const favorites = ref([])
 
 const getImageUrl = (path) => `https://image.tmdb.org/t/p/w500${path}`
 
-onMounted(() => {
+const loadFavorites = () => {
   const storedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]')
   favorites.value = storedFavorites
-})
+}
+
+const removeFromFavorites = (id) => {
+  const updatedFavorites = favorites.value.filter(movie => movie.id !== id)
+  localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+  favorites.value = updatedFavorites
+}
+
+onMounted(loadFavorites)
 </script>
