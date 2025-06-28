afe87cd8eca9b9909bc99cfef781e328ebabacfe
Jeremias <jeremias.ramirez@davinci.edu.ar>
2025-06-28 13:41:30 -0300

    se modifico el header para ser responsive, agregando un menu hamburguesa, ademas el titulo paso a ser Appelis

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,30 @@
 <template>
   <header class="header">
     <div class="container">
-      <router-link to="/" class="logo">🎬 Películas App</router-link>
-      <nav class="nav">
-        <router-link to="/" class="nav-link">Inicio</router-link>
-        <router-link to="/favoritos" class="nav-link">Favoritos</router-link>
+      <router-link to="/" class="logo">🎬 Appelis</router-link>
+
+      <button class="hamburger" @click="toggleMenu">
+        ☰
+      </button>
+
+      <nav :class="['nav', { open: menuOpen }]">
+        <router-link to="/" class="nav-link" @click="closeMenu">Inicio</router-link>
+        <router-link to="/favoritos" class="nav-link" @click="closeMenu">Favoritos</router-link>
       </nav>
     </div>
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
+}
+
+const closeMenu = () => {
+  menuOpen.value = false
+}
 </script>
